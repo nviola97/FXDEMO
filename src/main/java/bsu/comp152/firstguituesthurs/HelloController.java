6092ef0600243a41978f1fb7719f5a43fda1bf7d
N1VIOLA <n1viola@student.bridgew.edu>
2021-10-14 11:49:31 -0400

    messing around

--- src/main/java/bsu/comp152/firstguituesthurs/HelloController.java
+++ src/main/java/bsu/comp152/firstguituesthurs/HelloController.java
@@ -2,6 +2,8 @@ package bsu.comp152.firstguituesthurs;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 
 public class HelloController {
     @FXML
@@ -9,6 +11,10 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
+        var testColor= Paint.valueOf("Fuchsia");
+        var bigFont = Font.font(20);
+        welcomeText.setTextFill(testColor);
+        welcomeText.setFont(bigFont);
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
---
+++ src/main/java/bsu/comp152/firstguituesthurs/SecondWindow.java
@@ -0,0 +1,10 @@
+package bsu.comp152.firstguituesthurs;
+
+import javafx.fxml.FXML;
+
+public class SecondWindow {
+    @FXML
+    protected void HelloClicked(){
+
+    }
+}
